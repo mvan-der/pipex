a126464a32406d7ddd4757eb33a29d5d46a1ca2d
Maarten Van der vlugt <mvan-der@f1r2s1.codam.nl>
2022-04-20 12:53:46 +0200

    bla

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mvan-der <mvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/10 16:09:03 by mvan-der      #+#    #+#                 */
-/*   Updated: 2022/04/19 16:27:24 by mvan-der      ########   odam.nl         */
+/*   Updated: 2022/04/20 12:53:26 by mvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,28 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_file	pipex;
-	int		fd_in;
-	int		fd_out;
+	int		file_in;
+	int		file_out;
 	char	*binpath;
+	pid_t	pid;
 
 	if (input_check(argc, argv, &pipex) == -1)
 		return (-1);
-	fd_in = open(argv[1], O_RDONLY);
-	if (fd_in < 0)
+	file_in = open(argv[1], O_RDONLY);
+	if (file_in < 0)
 		return (OPEN_ERROR);
-	fd_out = open(argv[argc - 1], O_RDWR | O_TRUNC);
-	if (fd_out < 0)
+	file_out = open(argv[argc - 1], O_RDWR | O_TRUNC);
+	if (file_out < 0)
 	{
-		fd_out = open(argv[argc - 1], O_CREAT | O_WRONLY, 0644);
-		if (fd_out < 0)
+		file_out = open(argv[argc - 1], O_CREAT | O_WRONLY, 0644);
+		if (file_out < 0)
 			return (OPEN_ERROR);
 	}
-	if (!fork ())
+	pid = fork();
+	if (pid == 0)
 	{
-		dup2(fd_in, STDIN_FILENO);
-		dup2(fd_out, STDOUT_FILENO);
+		dup2(file_in, STDIN_FILENO);
+		dup2(file_out, STDOUT_FILENO); //-> this is output needed for 2nd command, atm redirected to std_out
 		if (access(pipex.command1[0], F_OK) == 0)
 			binpath = pipex.command1[0];
 		else
@@ -46,11 +48,11 @@ int	main(int argc, char **argv, char **envp)
 	}
 	else
 	{
-		close(fd_out);
+		close(file_out);
 		wait (NULL);
 	}
-	close(fd_out);
-	close(fd_in);
+	close(file_out);
+	close(file_in);
 	free(pipex.command1);
 	return (0);
 }
